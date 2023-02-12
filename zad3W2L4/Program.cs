1f40b6b0841d92fde868e25fbb334213c169505a
Ra v <timon521182@hotmail.com>
2023-02-12 21:40:30 +0100

    Add project files.

---
+++ zad3W2L4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace zad3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double a, b, c;
+            Console.WriteLine("Podaj szerokość prostokąta");
+
+            a = double.Parse(Console.ReadLine());                  
+
+            Console.WriteLine("Podaj wysokość prostokąta");
+
+            b = double.Parse(Console.ReadLine());
+            
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            Console.WriteLine($"Przekątna tego prostokąta wynosi {c}");
+
+            
+        }
+    }
+}
